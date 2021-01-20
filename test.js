121e3151e7ce5ede1eba478c87287e51af8dbc24
Drake Hobson <drakehobson2013@gmail.com>
2021-01-20 08:51:17 -0500

    yeet

---
+++ test.js
@@ -0,0 +1,9 @@
+// the first alert
+alert("Hello, World!");
+
+// the second alert
+alert("Hello, World!");
+
+// make the image invisible
+// move the iumage from top to bottom
+// make the image visible
